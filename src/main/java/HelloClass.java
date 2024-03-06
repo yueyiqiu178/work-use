2e2cc3e1ca34f52dfc0e0a661d00f758f08ab911
Max Qiu <yueyiqiu178@gmail.com>
2024-03-06 20:23:16 +0800

    add copy file

--- src/main/java/HelloClass.java
+++ src/main/java/HelloClass.java
@@ -1,5 +1,10 @@
 package main.java;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +15,28 @@ public class HelloClass {
 	}
 	private static final Logger logger = LoggerFactory.getLogger(HelloClass.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		logger.info("Hello Info");
 		logger.debug("Hello Debug");
 		logger.error("Hello Error");
+
+		File source = new File("D:/bg-imgame.zip");
+		File dest = new File("D:/abc/bg-imgame.zip");
+
+		Files.copy(source.toPath(), dest.toPath(),StandardCopyOption.REPLACE_EXISTING);
+		
+		File file = new File("D:/bg-imgame.zip");
+
+		if (file.delete()) {
+			System.out.println("File deleted successfully");
+		} else {
+			System.out.println("Failed to delete the file");
+		}
+
+//        Path copied = Paths.get("src/test/resources/copiedWithNio.txt");
+//        Path originalPath = original.toPath();
+//        Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
 
 	}
 }
